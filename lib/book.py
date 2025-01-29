f4856189e68d88351c3e0d72786119c6b89610d5
MICHELLEWAIRIMU04 <michelle@michelle>
2025-01-29 20:23:30 +0300

    first commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
     
+    def get_title(self):
+        return self._title
+
+    def set_title(self, title):
+        if isinstance(title, str):
+            self._title = title
+
+        else:
+            raise ValueError("Name must be a string")
+
+    title = property(get_title, set_title) 
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        
+        else:
+            raise ValueError("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def __str__(self):
+        return f"Book: {self.title}, Pages: {self.page_count}"
+
+book = Book("And Then There Were None", 272)
+book = Book("The World According to Garp", 69)
+print(book)
+book.turn_page()
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def  __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "used"
+
+    def get_brand(self):
+        self._brand 
+    
+    def set_brand(self, brand):
+        if isinstance(brand, str):
+            self._brand = brand
+        else:
+            raise ValueError("Brand must be a string")
+
+    brand = property(get_brand, set_brand)
+
+    def get_size(self):
+        return self._size 
+
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            raise ValueError("size must be an integer")
+        
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    
+    def __str__(self):
+        return f"Shoe(Brand: {self.brand}, Size: {self.size}, Condition: {self.condition})"  
+        
+
+shoe = Shoe("Adidas", 9)
+print(shoe)
+shoe.cobble()
+print(shoe)
